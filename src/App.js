0d1481d4778a43347851900aea1aa6cc3b26d85a
Nathan Stewart <stewart.nathang@gmail.com>
2017-10-27 09:28:19 -0700

    add eslint, create redux store w dummy reducer, render app with new text

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 
-export default class App extends Component<{}> {
+class App extends Component {
   render() {
     return (
-      <View />
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>
+            Hello!
+          </Text>
+        </View>
+      </Provider>
     );
   }
 }
+
+export default App;
